259dbff0c669013d0242a423ba4bd4bf4f081424
Elinaldo <elinnaldosc@gmail.com>
2024-08-27 14:35:52 -0300

    feat: add button to test seedDatabase and component to list product information

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { connectDb } from '@database/configs/connectDb';
+import { Product } from '@entities/Product';
+import { ListProductsService } from '@services/db/product/ListProductsService';
+import { SeedDatabaseService } from '@services/db/seeders/SeedDatabaseService';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [products, setProducts] = useState<Product[]>(null)
 
   useEffect(() => {
     const initializeDatabase = async () => {
@@ -13,10 +18,38 @@ export default function App() {
     initializeDatabase();
   }, []);
 
+  const getProducts = async() => {
+    try {
+      const products = await ListProductsService.execute();
+      setProducts(products);
+    } catch (error) {
+      console.log('Error fetching products: ', error.message);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Dev Estoque Sujeito P</Text>
+      <Text>Dev Estoque Sujeito Pro</Text>
+      <Button 
+        title='Populate Tables'  
+        onPress={async() => { 
+          await SeedDatabaseService.execute();
+          await getProducts();
+        }} 
+      />
       <StatusBar style="auto" />
+      <ScrollView>
+        {
+          products && products.map((product) => (
+            <View key={product.id} style = { styles.card }>
+              <Text>Product: {product.name}</Text>
+              <Text>Description: {product.description}</Text>
+              <Text>Category: {product.category.name}</Text>
+              <Text>Unit: {product.unit.code}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -24,8 +57,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 1
+  }
 });
